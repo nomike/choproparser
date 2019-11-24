9476629baa86a00396d23fd64a0668d6b6269713
nomike <nomike@nomike.com>
2019-11-24 18:30:40 +0100

    * rename module parse to parser * move main() to a separate script which imports "parser" * add GPL notice to python scripts

--- parser.py
+++ parser.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# ChoProParser - A parser which reads ChordPro files and converts them
+# into different formats using templates.
+# Copyright (C) 2019  nomike <nomike@nomike.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
-from mako.template import Template
 
 class SyntaxError(Exception):
     def __init__(self, expression, line_number, error):
@@ -226,17 +242,3 @@ def read_chopro(stream):
     logging.debug("Append last section to song")
     song.sections.append(section)
     return song
-
-
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    with open('example.cho') as file:
-        song = read_chopro(file)
-    template = Template(filename='chopro.template')
-    text = template.render(song=song)
-    print(text)
-    #print (song)
-
-
-if __name__ == "__main__":
-    main()
