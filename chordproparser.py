0116406e75e3e2b723f0a935d8206a217095abea
nomike <nomike@nomike.com>
2019-11-26 17:52:28 +0100

    * Add docstring for usage * Use docopt to parse usage * add option  for selecting the template * add option for the source chordpro file * add "--verbose" option to enable debug output

--- chordproparser.py
+++ chordproparser.py
@@ -4,29 +4,43 @@
 # choproparser - A parser which reads ChordPro files and converts them
 # into different formats using templates.
 # Copyright (C) 2019  nomike <nomike@nomike.com>
-
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Usage:
+chordproparser.py [-v|--verbose] [--template=<template>] <chordprofile>
+chordproparser.py -h | --help | --version
+"""
+
+
 
 from choproparser import parser
 import logging
+from docopt import docopt
 from mako.template import Template
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-    with open('example.cho') as file:
+    arguments = docopt(__doc__, version='0.1.0')
+    if arguments['-v'] or arguments['--verbose']:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("main")
+    template_file = arguments['--template'] if not arguments['--template'] == None else 'templates/__str__.template'
+    logger.debug("Selected template: %s" % (template_file))
+    with open(arguments['<chordprofile>']) as file:
         song = parser.read_chopro(file)
-    template = Template(filename='templates/__str__.template')
+    template = Template(filename=template_file)
     text = template.render(song=song)
     print(text)
     #print(song)
